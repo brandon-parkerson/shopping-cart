841515031e6714490161ee81bbceccea4339f861
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-02-10 16:13:57 +0200

    feat: shop link router

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'; 
 import logo from './assets/stock-increase-svgrepo-com.svg'
 import Cart from './Logo';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav>
-      <div><a href=''>Shop</a></div><div></div><img src={logo} alt="logo" className='logo' /><div className='check-out-nav'>
+      <div><Link to="shop">Shop</Link></div><div></div><img src={logo} alt="logo" className='logo' /><div className='check-out-nav'>
         <button className='check-out-btn'>Check out</button><Cart /></div>
       </nav>
   )
