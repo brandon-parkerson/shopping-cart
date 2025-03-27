b08f01805dce0b63fd229fd6da74c17344909594
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-03-27 16:03:02 +0200

    feat: cart total

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,9 +1,19 @@
 import { useOutletContext } from "react-router-dom";
 import "./Cart.css";
-import { CheckoutNav } from "./Nav";
+
 
 export default function Cart() {
   const { cartItems } = useOutletContext();
+    
+  function Total() {
+    let total = 0
+    cartItems.map((item) => {
+        total = total + item.price;
+    })
+    return (
+        <p>${total}</p>
+    )
+  }
 
   return (
     <>
@@ -17,9 +27,16 @@ export default function Cart() {
             <img src={item.image} className="item-img" />
             <div>{item.title}</div>
             <p>${item.price}</p>
+            
           </div>
+
         ))
       )}
+      <h2>Total</h2>
+      <Total />
+      
+      
     </>
+    
   );
 }
